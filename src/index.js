367e8042ec55ad965a144e525c279e8de64c864a
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-01 23:52:01 -0700

    set up mongodb and test connect with backend

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import express from "express";
 import path from "path";
-// import mongoose from "mongoose";
-// import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 // import dotenv from "dotenv";
 // import Promise from "bluebird";
 
-// import auth from "./routes/auth";
+import auth from "./routes/auth";
 // import users from "./routes/users";
 // import books from "./routes/books";
 
 // dotenv.config();
 const app = express();
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 // mongoose.Promise = Promise;
-// mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
+mongoose.connect("mongodb://localhost/bookworm", { useNewUrlParser: true });
 
-// app.use("/api/auth", auth);
+app.use("/api/auth", auth);
 // app.use("/api/users", users);
 // app.use("/api/books", books);
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({errors: {global: "Invalid credentials"}});
-});
+// app.post("/api/auth", (req, res) => {
+//   res.status(400).json({errors: {global: "Invalid credentials"}});
+// });
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
