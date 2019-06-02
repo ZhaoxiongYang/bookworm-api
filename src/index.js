a492b81a3a1f3edb4d2ae5dc49975041fcb9be40
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-02 16:54:32 -0700

    setup book routes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Promise from "bluebird";
 
 import auth from "./routes/auth";
 import users from "./routes/users";
-// import books from "./routes/books";
+import books from "./routes/books";
 
 dotenv.config();
 const app = express();
@@ -17,11 +17,8 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 
 app.use("/api/auth", auth);
 app.use("/api/users", users);
-// app.use("/api/books", books);
+app.use("/api/books", books);
 
-// app.post("/api/auth", (req, res) => {
-//   res.status(400).json({errors: {global: "Invalid credentials"}});
-// });
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
