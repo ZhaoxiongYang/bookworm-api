c3f0e43c6557008a4ce93e16d374742106f730da
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-02 18:25:27 -0700

    connect to goodreads api

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,9 +1,9 @@
 import express from "express";
-// import request from "request-promise";
-// import { parseString } from "xml2js";
+import request from "request-promise";
+import { parseString } from "xml2js";
 import authenticate from "../middlewares/authenticate";
 // import Book from "../models/Book";
-import parseErrors from "../utils/parseErrors";
+// import parseErrors from "../utils/parseErrors";
 
 const router = express.Router();
 router.use(authenticate);
@@ -18,62 +18,44 @@ router.use(authenticate);
 // });
 
 router.get("/search", (req, res) => {
-  res.json({
-    books:[{
-      goodreadsId: 1,
-      title: "1984",
-      authors: "orwell",
-      covers:["https://embed.cdn.pais.scholastic.com/v1/systems/kids/products/identifiers/isbn/9781338174526/cover/renditions/900",
-              "https://images.gr-assets.com/books/1433161048l/1137215.jpg"],
-      pages: 198
-    },{
-      goodreadsId: 2,
-      title: "three codes",
-      authors: "alex",
-      covers:["http://images.gr-asserts.com/books/13489905661/5470.jpg",
-              "http://images.gr-asserts.com/books/15046119571/9577857.jpg"],
-      pages: 565
-    }]
-  })
-
-  // request
-  //   .get(
-  //     `https://www.goodreads.com/search/index.xml?key=${process.env
-  //       .GOODREADS_KEY}&q=${req.query.q}`
-  //   )
-  //   .then(result =>
-  //     parseString(result, (err, goodreadsResult) =>
-  //       res.json({
-  //         books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
-  //           work => ({
-  //             goodreadsId: work.best_book[0].id[0]._,
-  //             title: work.best_book[0].title[0],
-  //             authors: work.best_book[0].author[0].name[0],
-  //             covers: [work.best_book[0].image_url[0]]
-  //           })
-  //         )
-  //       })
-  //     )
-  //   );
+  request
+    .get(
+      `https://www.goodreads.com/search/index.xml?key=${process.env
+        .GOODREADS_KEY}&q=${req.query.q}`
+    )
+    .then(result =>
+      parseString(result, (err, goodreadsResult) =>
+        res.json({
+          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+            work => ({
+              goodreadsId: work.best_book[0].id[0]._,
+              title: work.best_book[0].title[0],
+              authors: work.best_book[0].author[0].name[0],
+              covers: [work.best_book[0].image_url[0]]
+            })
+          )
+        })
+      )
+    );
 });
 
-// router.get("/fetchPages", (req, res) => {
-//   const goodreadsId = req.query.goodreadsId;
+router.get("/fetchPages", (req, res) => {
+  const goodreadsId = req.query.goodreadsId;
 
-//   request
-//     .get(
-//       `https://www.goodreads.com/book/show.xml?key=${process.env
-//         .GOODREADS_KEY}&id=${goodreadsId}`
-//     )
-//     .then(result =>
-//       parseString(result, (err, goodreadsResult) => {
-//         const numPages = goodreadsResult.GoodreadsResponse.book[0].num_pages[0];
-//         const pages = numPages ? parseInt(numPages, 10) : 0;
-//         res.json({
-//           pages
-//         });
-//       })
-//     );
-// });
+  request
+    .get(
+      `https://www.goodreads.com/book/show.xml?key=${process.env
+        .GOODREADS_KEY}&id=${goodreadsId}`
+    )
+    .then(result =>
+      parseString(result, (err, goodreadsResult) => {
+        const numPages = goodreadsResult.GoodreadsResponse.book[0].num_pages[0];
+        const pages = numPages ? parseInt(numPages, 10) : 0;
+        res.json({
+          pages
+        });
+      })
+    );
+});
 
 export default router;
