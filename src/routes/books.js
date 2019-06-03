5551357d253572442e68465285416cb902456017
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-02 18:02:25 -0700

    set up autnorize

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,12 +1,12 @@
 import express from "express";
 // import request from "request-promise";
 // import { parseString } from "xml2js";
-// import authenticate from "../middlewares/authenticate";
+import authenticate from "../middlewares/authenticate";
 // import Book from "../models/Book";
 import parseErrors from "../utils/parseErrors";
 
 const router = express.Router();
-// router.use(authenticate);
+router.use(authenticate);
 // router.get("/", (req, res) => {
 //   Book.find({ userId: req.currentUser._id }).then(books => res.json({ books }));
 // });
